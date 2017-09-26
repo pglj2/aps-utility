ac5a06747921d3ebae442e4aaf03e3fb2d923dbe
pglj2 <pglj2@cin.ufpe.br>
2017-09-25 22:12:35 -0300

    adicionado inicio do projeto no framework Play

---
+++ mpmanager/build.sbt
@@ -0,0 +1,10 @@
+name := """MPManager"""
+organization := "cin"
+
+version := "1.0-SNAPSHOT"
+
+lazy val root = (project in file(".")).enablePlugins(PlayJava)
+
+scalaVersion := "2.12.2"
+
+libraryDependencies += guice
---
+++ mpmanager/target/scala-2.12/routes/main/controllers/routes.java
@@ -0,0 +1,21 @@
+
+// @GENERATOR:play-routes-compiler
+// @SOURCE:C:/Users/Avell/Desktop/mpman/aps-utility/mpmanager/conf/routes
+// @DATE:Mon Sep 25 21:13:31 GFT 2017
+
+package controllers;
+
+import router.RoutesPrefix;
+
+public class routes {
+  
+  public static final controllers.ReverseHomeController HomeController = new controllers.ReverseHomeController(RoutesPrefix.byNamePrefix());
+  public static final controllers.ReverseAssets Assets = new controllers.ReverseAssets(RoutesPrefix.byNamePrefix());
+
+  public static class javascript {
+    
+    public static final controllers.javascript.ReverseHomeController HomeController = new controllers.javascript.ReverseHomeController(RoutesPrefix.byNamePrefix());
+    public static final controllers.javascript.ReverseAssets Assets = new controllers.javascript.ReverseAssets(RoutesPrefix.byNamePrefix());
+  }
+
+}
