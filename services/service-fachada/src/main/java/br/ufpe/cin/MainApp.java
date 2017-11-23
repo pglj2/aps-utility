0cf5e7584f8142e4822b8bd58ccb6e3912f72a0f
TMotaBastos <thiago.bastos972@gmail.com>
2017-11-22 23:37:12 -0200

    feat(service-fachada): add fachada service

---
+++ services/service-fachada/src/main/java/br/ufpe/cin/MainApp.java
@@ -0,0 +1,18 @@
+package br.ufpe.cin;
+
+import br.ufpe.cin.resources.BuscarPorNomeResource;
+import br.ufpe.cin.resources.CadastrarAtividadeResource;
+import io.dropwizard.Application;
+import io.dropwizard.setup.Environment;
+
+public class MainApp extends Application<ServiceFachadaConfiguration> {
+    public static void main(final String[] args) throws Exception {
+        new MainApp().run(args);
+    }
+
+    @Override
+    public void run(ServiceFachadaConfiguration configuration, Environment environment) throws Exception {
+        environment.jersey().register(new BuscarPorNomeResource());
+        environment.jersey().register(new CadastrarAtividadeResource());
+    }
+}
---
+++ services/service-fachada/src/main/java/br/ufpe/cin/resources/BuscarPorNomeResource.java
@@ -0,0 +1,28 @@
+package br.ufpe.cin.resources;
+
+import io.dropwizard.jersey.params.NonEmptyStringParam;
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.io.IOException;
+
+@Path("/buscar/usuario")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
+public class BuscarPorNomeResource {
+    @GET
+    public Response buscarPorNome(@QueryParam("nome") NonEmptyStringParam nome) throws IOException {
+        String url = "http://servicecadastrousuario:8080/buscar/usuario?nome=" + nome.get().get();
+        String json = Jsoup.connect(url)
+                           .method(Connection.Method.GET)
+                           .ignoreContentType(true)
+                           .execute()
+                           .body();
+        //Document doc = Jsoup.connect(url).get();
+        //return Response.ok(doc.body()).build();
+        return Response.ok(json).build();
+    }
+}
